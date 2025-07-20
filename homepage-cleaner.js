b146a6b1e0cad6a231dfe658b8051b29d29a0305
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-07-20 19:04:37 -0400

    Refactor section removal code

--- homepage-cleaner.js
+++ homepage-cleaner.js
@@ -1,19 +1,26 @@
 const firstSections = document.querySelectorAll('.game-sort-carousel-wrapper');
 const recomendedSections = document.querySelectorAll('[data-testid="home-page-game-grid"]');
+const highlightTitleClass = '.css-1h1fine-titleSubtitleContainer';
 
 document.body.style.border = "5px solid green"; // Just to confirm the script is running
 
-function updateSections() {
-    firstSections.forEach(section => {
-        const titleElement = section.querySelector(".css-1h1fine-titleSubtitleContainer");
-        console.log(titleElement + "  removed!");
-        if (titleElement && titleElement.ariaLabel.includes("Today's Picks")) {
+function removeSection(section, checkTitle = false, titleText = "") {
+    if (checkTitle && titleText !== "") {
+        const titleElement = section.querySelector(highlightTitleClass);
+        if (titleElement && titleElement.ariaLabel.includes(titleText)) {
             section.style.display = "none";
         }
+    } else {
+        section.style.display = "none";
+    }
+}
+
+function updateSections() {
+    firstSections.forEach(section => {
+        removeSection(section, true, "Today's Picks");
     });
     recomendedSections.forEach(section => {
-        section.style.display = "none";
-        console.log(section + "  removed!");
+        removeSection(section);
     });
 }
 
