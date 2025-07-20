7d931dca17b9de2cef07cf0b6cf2e170b3f96b86
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-07-20 19:15:07 -0400

    Fix not working on reload by updating any time there is a change

--- homepage-cleaner.js
+++ homepage-cleaner.js
@@ -1,5 +1,3 @@
-const firstSections = document.querySelectorAll('.game-sort-carousel-wrapper');
-const recomendedSections = document.querySelectorAll('[data-testid="home-page-game-grid"]');
 const highlightTitleClass = '.css-1h1fine-titleSubtitleContainer';
 
 document.body.style.border = "5px solid green"; // Just to confirm the script is running
@@ -15,7 +13,7 @@ function removeSection(section, checkTitle = false, titleText = "") {
     }
 }
 
-function updateSections() {
+function updateSections(firstSections, recomendedSections) {
     firstSections.forEach(section => {
         removeSection(section, true, "Today's Picks");
     });
@@ -24,4 +22,12 @@ function updateSections() {
     });
 }
 
-updateSections();
+const observer = new MutationObserver((mutationsList, _) => {
+    let firstSections = document.querySelectorAll('.game-sort-carousel-wrapper');
+    let recomendedSections = document.querySelectorAll('[data-testid="home-page-game-grid"]');
+
+    updateSections(firstSections, recomendedSections);
+    console.log("Homepage sections updated");
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
