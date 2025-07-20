1119877e440262db246151ec1c4f6b19dbe5a57b
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-07-20 19:55:32 -0400

    Add settings & popup code

--- content_scripts/homepage-cleaner.js
+++ content_scripts/homepage-cleaner.js
@@ -1,5 +1,8 @@
 const highlightTitleClass = '.css-1h1fine-titleSubtitleContainer';
 
+let removeHighlights = true;
+let removeReccomended = true;
+
 function removeSection(section, checkTitle = false, titleText = "") {
     if (checkTitle && titleText !== "") {
         const titleElement = section.querySelector(highlightTitleClass);
@@ -12,20 +15,35 @@ function removeSection(section, checkTitle = false, titleText = "") {
 }
 
 function updateSections(firstSections, recomendedSections) {
-    firstSections.forEach(section => {
-        removeSection(section, true, "Today's Picks");
-    });
-    recomendedSections.forEach(section => {
-        removeSection(section);
-    });
+    if (!removeHighlights && !removeReccomended) return;
+    if (removeHighlights) {
+        firstSections.forEach(section => {
+            removeSection(section, true, "Today's Picks");
+        });
+    }
+    if (removeReccomended) {
+        recomendedSections.forEach(section => {
+            removeSection(section);
+        });
+    }
 }
 
-const observer = new MutationObserver(() => {
+function init() {
     let firstSections = document.querySelectorAll('.game-sort-carousel-wrapper');
     let recomendedSections = document.querySelectorAll('[data-testid="home-page-game-grid"]');
 
     updateSections(firstSections, recomendedSections);
     console.log("Homepage sections updated");
-});
+}
 
+const observer = new MutationObserver(init);
 observer.observe(document.body, { childList: true, subtree: true });
+
+browser.runtime.onMessage.addListener((message) => {
+    if (message.type === "updateCleanerSettings") {
+        removeHighlights = message.removeHighlights;
+        removeReccomended = message.removeReccomended;
+
+        init();
+    }
+});
---
+++ popup/homepage_cleaner_settings.js
@@ -0,0 +1,20 @@
+function updateSettings() {
+    const removeHighlights = document.querySelector("#remove-highlights").checked;
+    const removeReccomended = document.querySelector("#remove-recommended").checked;
+
+    browser.tabs.query({ active: true, currentWindow: true }).then((tabs) => {
+        browser.tabs.sendMessage(tabs[0].id, {
+            type: "updateCleanerSettings",
+            removeHighlights: removeHighlights,
+            removeReccomended: removeReccomended
+        });
+    });
+}
+
+function reportExecuteScriptError(error) {
+    document.querySelector("#popup-content").classList.add("hidden");
+    document.querySelector("#error-content").classList.remove("hidden");
+    console.error(`Failed to execute homepage-cleaner content script: ${error.message}`);
+}
+
+browser.tabs.executeScript({ File: "content_scripts/homepage-cleaner.js" }).then().catch(reportExecuteScriptError);
