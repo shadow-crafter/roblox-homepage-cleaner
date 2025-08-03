da3ab3cd1e22011145185ef2f578a3b5caec1228
shadowcrafter <65373035+shadow-crafter@users.noreply.github.com>
2025-08-03 11:02:02 -0400

    Reformat things, remove magic variables in main script

--- content_scripts/homepage-cleaner.js
+++ content_scripts/homepage-cleaner.js
@@ -1,48 +1,52 @@
-const highlightTitleClass = '.css-1h1fine-titleSubtitleContainer';
+const highlightTitleClass = ".css-1h1fine-titleSubtitleContainer";
+const firstSectionsClass = ".game-sort-carousel-wrapper";
+const reccomendedSectionsId = '[data-testid="home-page-game-grid"]';
 
 let removeHighlights = false;
 let removeRecommended = false;
 
 function removeSection(section, checkTitle = false, titleText = "") {
-    if (checkTitle && titleText !== "") {
-        const titleElement = section.querySelector(highlightTitleClass);
-        if (titleElement && titleElement.ariaLabel.includes(titleText)) {
-            section.style.display = "none";
-        }
-    } else {
-        section.style.display = "none";
+  if (checkTitle && titleText !== "") {
+    const titleElement = section.querySelector(highlightTitleClass);
+    if (titleElement && titleElement.ariaLabel.includes(titleText)) {
+      section.style.display = "none";
     }
+  } else {
+    section.style.display = "none";
+  }
 }
 
 function updateSections(firstSections, recommendedSections) {
-    if (!removeHighlights && !removeRecommended) return;
-    if (removeHighlights) {
-        firstSections.forEach(section => {
-            removeSection(section, true, "Today's Picks");
-        });
-    }
-    if (removeRecommended) {
-        recommendedSections.forEach(section => {
-            removeSection(section);
-        });
-    }
+  if (!removeHighlights && !removeRecommended) return;
+  if (removeHighlights) {
+    firstSections.forEach((section) => {
+      removeSection(section, true, "Today's Picks");
+    });
+  }
+  if (removeRecommended) {
+    recommendedSections.forEach((section) => {
+      removeSection(section);
+    });
+  }
 }
 
 function updateSettings(settings) {
-    removeHighlights = settings.removeHighlights;
-    removeRecommended = settings.removeRecommended;
-    init();
+  removeHighlights = settings.removeHighlights;
+  removeRecommended = settings.removeRecommended;
+  init();
 }
 
 function init() {
-    let firstSections = document.querySelectorAll('.game-sort-carousel-wrapper');
-    let recommendedSections = document.querySelectorAll('[data-testid="home-page-game-grid"]');
+  let firstSections = document.querySelectorAll(firstSectionsClass);
+  let recommendedSections = document.querySelectorAll(reccomendedSectionsId);
 
-    updateSections(firstSections, recommendedSections);
-    console.log("Homepage sections updated");
+  updateSections(firstSections, recommendedSections);
+  console.log("Homepage sections updated");
 }
 
 const observer = new MutationObserver(init);
 observer.observe(document.body, { childList: true, subtree: true });
 
-browser.storage.sync.get(["removeHighlights", "removeRecommended"]).then(updateSettings);
+browser.storage.sync
+  .get(["removeHighlights", "removeRecommended"])
+  .then(updateSettings);
--- options/options.js
+++ options/options.js
@@ -1,23 +1,28 @@
 function saveOptions(e) {
-    e.preventDefault();
-    browser.storage.sync.set({
-        removeHighlights: document.querySelector("#remove-highlights").checked,
-        removeRecommended: document.querySelector("#remove-recommended").checked
-    });
+  e.preventDefault();
+  browser.storage.sync.set({
+    removeHighlights: document.querySelector("#remove-highlights").checked,
+    removeRecommended: document.querySelector("#remove-recommended").checked,
+  });
 }
 
 function restoreOptions() {
-    function setCurrentChoice(result) {
-        document.querySelector("#remove-highlights").checked = result.removeHighlights;
-        document.querySelector("#remove-recommended").checked = result.removeRecommended;
-    }
+  function setCurrentChoice(result) {
+    document.querySelector("#remove-highlights").checked =
+      result.removeHighlights;
+    document.querySelector("#remove-recommended").checked =
+      result.removeRecommended;
+  }
 
-    function onError(error) {
-        console.error(`Error: ${error}`);
-    }
+  function onError(error) {
+    console.error(`Error: ${error}`);
+  }
 
-    let getting = browser.storage.sync.get({"removeHighlights": true, "removeRecommended": true});
-    getting.then(setCurrentChoice, onError);
+  let getting = browser.storage.sync.get({
+    removeHighlights: true,
+    removeRecommended: true,
+  });
+  getting.then(setCurrentChoice, onError);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
